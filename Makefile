6236f4fa80f4534c180c2165ba87a5cc701d55b7
bing.li <libinglimit@gmail.com>
2017-04-26 07:57:56 -0700

    add the encoder

--- Makefile
+++ Makefile
@@ -2,9 +2,10 @@ all:
 	gcc -g demuxing_decoding.c -o demuxing_decoding -L/usr/local/lib -I/usr/local/include -lavformat -lavdevice -lavcodec -lswscale -lswresample -lavutil -lavfilter -lpthread -lz -lm -lva
 	gcc -g metadata.c -o metadata -L/usr/local/lib -I/usr/local/include -lavformat -lavdevice -lavcodec -lswscale -lswresample -lavutil -lavfilter -lpthread -lz -lm -lva
 	gcc -g container_parse.c -o container_parse -L/usr/local/lib -I/usr/local/include -lavformat -lavdevice -lavcodec -lswscale -lswresample -lavutil -lavfilter -lpthread -lz -lm -lva
+	gcc -g encoder.c -o encoder -L/usr/local/lib -I/usr/local/include -lavformat -lavdevice -lavcodec -lswscale -lswresample -lavutil -lavfilter -lpthread -lz -lm -lva
 
 clean:
-	rm demuxing_decoding metadata container_parse
+	rm demuxing_decoding metadata container_parse encoder
 	rm videodst audiodst
 
 .PHONY:clean
---
+++ encoder.c
@@ -0,0 +1,162 @@
+#include <stdio.h>
+#include <libavformat/avformat.h>
+#include <libavcodec/avcodec.h>
+#include <libavutil/avutil.h>
+
+static char *InputFileName  = NULL;
+static char *OutputFileName = NULL;
+static FILE *pInput_File     = NULL;
+static FILE *pOutput_File    = NULL;
+
+static int frameWidth    = 0;
+static int frameHeight   = 0;
+static int bitRate       = 0;
+static int frameToEncode = 0;
+
+void parse_argv(char **argv)
+{
+    InputFileName  = argv[1];
+    OutputFileName = argv[2];
+
+    pInput_File = fopen(InputFileName, "rb+");
+    if(!pInput_File){
+        fprintf(stderr, "open input file fail\n"); 
+        return;
+    }
+
+    pOutput_File = fopen(OutputFileName, "wb+");
+    if(!pOutput_File){
+        fprintf(stderr, "open output file fail\n");
+        return;
+    }
+
+    return;
+}
+
+int main(int argc, char **argv)
+{
+    int ret = 0;
+    int i, x, y;
+    int got_output;
+    AVCodec *codec = NULL;
+    AVCodecContext *codecCtx = NULL;
+    AVFrame *frame = NULL;
+    AVPacket pkt;
+
+    if(argc != 3){
+        fprintf(stderr, "Usage:%s <inputfilename> <outputfilename> \n", 
+                argv[0]); 
+        exit(1);
+    }
+
+    //parse argument
+    parse_argv(argv);
+
+    av_register_all();
+    avcodec_register_all();
+
+    codec = avcodec_find_encoder(AV_CODEC_ID_MPEG2VIDEO);
+    if(!codec){
+        fprintf(stderr, "Could not find the encoder\n");
+        return -1;
+    }
+
+    codecCtx = avcodec_alloc_context3(codec);
+    if(!codecCtx){
+        fprintf(stderr, "Could not allocate video codec context\n");
+        return -1;
+    }
+
+    codecCtx->bit_rate = 400000;
+    codecCtx->width    = 352;
+    codecCtx->height   = 288;
+    codecCtx->time_base = (AVRational){1, 25};
+    codecCtx->gop_size  = 10;
+    codecCtx->max_b_frames = 1;
+    codecCtx->pix_fmt = AV_PIX_FMT_YUV420P;
+
+    av_opt_set(codecCtx->priv_data, "preset", "slow", 0);
+
+    //open the encoder
+    if(avcodec_open2(codecCtx, codec, NULL) < 0){
+        fprintf(stderr, "Open encoder fail\n");
+        return -1;
+    }
+
+    //allocate AVFrame structure
+    frame = av_frame_alloc();
+    if(!frame){
+        fprintf(stderr, "Could not allocate wideo frame\n"); 
+        return -1;
+    }
+    frame->format  = codecCtx->pix_fmt;
+    frame->width   = codecCtx->width;
+    frame->height  = codecCtx->height;
+
+    //allocate AVFrame data 
+    ret = av_image_alloc(frame->data, frame->linesize, codecCtx->width, codecCtx->height,
+                         codecCtx->pix_fmt, 32);
+    if(ret < 0){
+        fprintf(stderr, "Could not allocate raw picture buffer\n");
+        return -1;
+    }
+
+
+    for(i = 0; i < 25; i++){
+        //init AVPacket
+        av_init_packet(&pkt); 
+        pkt.data = NULL;
+        pkt.size = 0;
+
+        fflush(stdout);
+        //prepare a dummy image
+        /*Y*/
+        for(y = 0; y < codecCtx->height; y++){
+            for(x = 0; x < codecCtx->width; x++){
+                frame->data[0][y*frame->linesize[0] + x] = x + y + i*3;
+            }
+        }
+
+        /*Cb and Cr*/
+        for(y = 0; y < codecCtx->height/2; y++){
+            for(x = 0; x < codecCtx->width/2; x++){
+                frame->data[1][y*frame->linesize[1] + x] = 128 + y + i*2;
+                frame->data[2][y*frame->linesize[2] + x] = 64 + x + i*5;
+            }
+        }
+
+        frame->pts = i;
+
+        //encode the image
+        ret = avcodec_encode_video2(codecCtx, &pkt, frame, &got_output);
+        if(ret < 0){
+            fprintf(stderr, "Error encoding frame\n");
+            return -1;
+        }
+
+        if(got_output){
+            printf("Write frame %3d (size=%5d) \n", i, pkt.size);
+            fwrite(pkt.data, 1, pkt.size, pOutput_File);
+            av_free_packet(&pkt);
+        }
+    }
+
+    //get the delayed frames
+    for(got_output = 1; got_output;){
+        fflush(stdout);
+
+        ret = avcodec_encode_video2(codecCtx, &pkt, NULL, &got_output);
+        if(ret < 0){
+            fprintf(stderr, "Error encoding frame\n");
+            return -1;
+        }
+
+        if(got_output){
+            printf("Write frame %3d (size=%5d) \n", i, pkt.size);
+            fwrite(pkt.data, 1, pkt.size, pOutput_File);
+            av_free_packet(&pkt);
+        }
+    }
+
+    return 0;
+}
